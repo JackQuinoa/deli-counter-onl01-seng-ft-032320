6f2ffe282d394afcd688c201289c0dac7d1152b6
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:25:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 # Write your code here.
 
 katz_deli = []
-def line
+def line(order)
   if katz_deli.count = 0
     return "The line is currently empty."
   else 
-    return katz_deli.
+    return katz_deli.count
 end
 
-def 
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
