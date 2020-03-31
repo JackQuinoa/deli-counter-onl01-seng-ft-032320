666a3d1c0213fc5b9066c1d61f17d47de735b7b3
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:31:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,24 @@
 require "pry"
 katz_deli = []
 def line(order)
-  if katz_deli.count = 0
+  if katz_deli.size = 0
     return "The line is currently empty."
   else 
     return katz_deli.count
-    binding.pry
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli} in line."
