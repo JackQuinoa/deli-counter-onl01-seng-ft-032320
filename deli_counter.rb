73d79aa71940cbf003410cef5e747cd412559292
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:28:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,18 @@ def line(order)
     return "The line is currently empty."
   else 
     return katz_deli.count
+  end
 end
 
 def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
+
+def now_serving(person)
+  if katz_deli.count=0
+    puts "There is nobody waiting to be served!"
+  else
+    puts #{person}
+    katz_deli.shift
+  end
 end
