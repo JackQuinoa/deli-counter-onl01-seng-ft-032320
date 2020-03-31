af9a7b6bc51c34cc30bca53dfbe79669fb609a9b
Jack Quinoa <jquinoa208@gmail.com>
2020-03-31 21:40:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,34 +4,9 @@ require "pry"
 katz_deli = []
 
 def line(order)
-  if katz_deli = 0
+  if katz_deli.count = 0
     puts "The line is currently empty."
   else 
     puts "
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli} in line."
-end
-
-def now_serving(person)
-  if katz_deli.count = 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts #{person}
-    katz_deli.shift
   end
 end
